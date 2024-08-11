a7a6928c51bfe702ced7f9b740c226c15452bacb
Marcos Paulo <marcos.p.santoos@gmail.com>
2024-08-11 20:10:32 -0300

    Criação do projeto next e instalação do Disign System

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Heading, Text } from "@ignite-ui/react";
+
+export default function homw() {
+  return (
+    <>
+      <Heading as="h1">Hellow Word</Heading>
+      <Text>Hellow Word</Text>
+    </>
+  );
+}
